cf80809203406d117bbdd17c2013bf56e03f6d2a
shiv kumar <shiv@riversands-MacBook-Pro.local>
2020-05-04 22:34:46 +0530

    log v0.1

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,11 @@ import (
 	socketio "github.com/googollee/go-socket.io"
 )
 
+type SocketWithLock struct {
+	Socket *socketio.Conn
+	Lock   *sync.Mutex
+}
+
 func Contains(arr []string, str string) bool {
 	for _, a := range arr {
 		if a == str {
@@ -15,7 +20,10 @@ func Contains(arr []string, str string) bool {
 	return false
 }
 
-type SocketWithLock struct {
-	Socket *socketio.Conn
-	Lock   *sync.Mutex
+func Log(tenantId string, calleeServiceName string, userId string, message string) {
+	// "requestId", "guid", "tenantId", "callerServiceName", "calleeServiceName",
+	// "relatedRequestId", "groupRequestId", "taskId", "userId", "entityId",
+	// "objectType", "className", "method", "newTimestamp", "action",
+	// "inclusiveTime", "messageCode", "instanceId", "logMessage"
+	//var messageTemplate string = `[] [] [` + tenantId + `] [Go-Notification] [` + calleeServiceName + `] [] [] [] [` + userId + `] [] [] [] [] [] [] [] [] [] [` + message + `]`
 }
